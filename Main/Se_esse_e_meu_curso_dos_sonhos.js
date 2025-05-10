bb570641b7f83f2f37ddef873c8e5c65d8468f65
Miguel Damazio <grishnakhtheoden@gmail.com>
2025-05-10 13:25:33 -0300

    Atualização 3
    Isso vai dar certo, mas chega por enquanto.

--- Main/Se_esse_e_meu_curso_dos_sonhos.js
+++ Main/Se_esse_e_meu_curso_dos_sonhos.js
@@ -1,11 +1,11 @@
- alert ("Um Pequeno Tour pela história do melhor país do mundos");
+
 
  function btnclicka() {
  alert ("Brasil");
 }
 
 function btnclick() {
-alert ("India");
+alert ("Paquistão");
 
 }
 
